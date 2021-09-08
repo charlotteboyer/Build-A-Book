b19478e892bc5d196159d496015979077b6fdaa4
Charlotte <charlottecboyer@gmail.com>
2021-09-08 12:10:17 -0400

    api functioning and map functioning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'; 
 
 function App() {
-  return (
+
+  const [ query, setQuery ] = useState("");
+
+  const [ search, setSearch ] =useState("");
+
+  const [ dataObj, setDataObj ] = useState([]);
+
+  const [ loaded, setLoaded ] = useState( true )
+ 
+  useEffect( () => {
+
+    fetch(`http://openlibrary.org/search.json?q=${search}`)
+      .then( response => response.json())
+      .then( (data) => {
+        setDataObj(data);
+        setLoaded(false);
+      })
+    
+    
+    }, [search])
+
+
+  console.log(dataObj)
+
+
+  return !loaded ? 
+  (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Build A Book: Book Search</h1>
+      <form >
+        <input 
+        type="text" 
+        value={query}
+        onChange={(e) => {setQuery(e.target.value)}}/>
+        <button
+        type="button"
+        onClick={() => setSearch(query)}>submit</button>
+      </form>
+
+
+      <ul>
+      {
+        dataObj.docs.map((res) => {
+          return (
+            <li key={res.key}>
+              <h2>{res.title}</h2>
+              <p>{res.author_name}</p>
+            </li>
+          )})
+      }
+        
+      
+      </ul>
+      
+
+
+
     </div>
-  );
+    ) : (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
 }
 
 export default App;
